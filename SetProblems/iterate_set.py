c93438dd1569471240b72e69c24f5bfaf5d77337
Vemula Dileep <vemuladileep082002@gmail.com>
2024-09-25 15:23:06 +0530

    [VEMULA] add: Python program to iterate over a set

---
+++ SetProblems/iterate_set.py
@@ -0,0 +1,26 @@
+'''
+    @Author: VEMULA DILEEP
+    @Date: 24-09-2024
+    @Last Modified by: VEMULA DILEEP
+    @Last Modified time: 20:05
+    @Title : Python program to iterate over a set
+'''
+
+def iterate_set():
+    """
+    Description:
+        This function iterates over a set and prints its elements.
+    Parameters:
+        None
+    Return:
+        None
+    """
+    my_set = {1, 2, 3, 4, 5}
+    for element in my_set:
+        print(element)
+
+def main():
+    iterate_set()
+
+if __name__ == "__main__":
+    main()
