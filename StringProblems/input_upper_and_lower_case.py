83477eba374bc67f8c31e6559df80c5bf3e66f41
Vemula Dileep <vemuladileep082002@gmail.com>
2024-09-30 13:08:26 +0530

    [VEMULA add : python program to display user input in upper and lower cases.]

---
+++ StringProblems/input_upper_and_lower_case.py
@@ -0,0 +1,15 @@
+'''
+    @Author: VEMULA DILEEP
+    @Date: 30-09-2024
+    @Last Modified by: VEMULA DILEEP
+    @Last Modified time: 16:35
+    @Title : Python script to display user input in upper and lower cases.
+'''
+
+def main():
+    user_input = input("Enter a string: ")
+    print("Uppercase:", user_input.upper())
+    print("Lowercase:", user_input.lower())
+
+if __name__ == "__main__":
+    main()
